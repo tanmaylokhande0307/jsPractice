e82defa1b35e60a7b09f16b0fa32497d707e05e8
Tanmay <tanmaynl37@gmail.com>
2022-12-07 09:21:26 +0530

    classes

---
+++ classes/index.js
@@ -0,0 +1,10 @@
+class Person {
+    constructor(firstName,lastName,age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+}
+
+let tan = new Person("Tanmay","Lokhande",23);
+console.log(tan);
